45afaffd42f351e1f4ce6b4bb077ec244c590423
alonecreations <108176909+alonecreations@users.noreply.github.com>
2025-02-26 22:14:30 +0530

    git class

---
+++ app.js
@@ -0,0 +1,19 @@
+// a = 10 
+// b = 20
+// console.log("Sum of a and b is " ,a+b)
+
+// a = "10";
+// a++
+// console.log(a);
+// type coercion agar kisi number ko string bna ke mathematical task perform krte hai to vo string ka number me 
+//  type coercion ho jata hai aur vo number ke jaise behave krne lgta hai
+// let a =1
+// let b=2 
+// let c 
+// c  = a ;
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
